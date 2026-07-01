f8f178cea6176f90c630024e3d79e5da5f236918
Zhang Shijie <z1332556430@gmail.com>
2026-07-01 19:37:36 +0800

    Build preview for refined airport groups

--- convert.js
+++ convert.js
@@ -72,7 +72,7 @@ https://github.com/powerfullz/override-rules
   });
 
   // src/constants.ts
-  var NODE_SUFFIX, CDN_URL, SPEEDTEST_URL, LOW_COST_NODE_MATCHER, FLOWER_PREMIUM_ASIA_NODE_MATCHER, SINGAPORE_FLOWER_TWG_NODE_MATCHER, JAPAN_FLOWER_TWG_NODE_MATCHER, PROXY_GROUPS, countriesMeta;
+  var NODE_SUFFIX, CDN_URL, SPEEDTEST_URL, LOW_COST_NODE_MATCHER, FLOWER_PREMIUM_ASIA_NODE_MATCHER, SINGAPORE_FLOWER_TWG_NODE_MATCHER, JAPAN_FLOWER_TWG_NODE_MATCHER, EXPERIMENTAL_NODE_MATCHER, PROXY_GROUPS, countriesMeta;
   var init_constants = __esm({
     "src/constants.ts"() {
       "use strict";
@@ -92,6 +92,7 @@ https://github.com/powerfullz/override-rules
       JAPAN_FLOWER_TWG_NODE_MATCHER = createCaseInsensitiveNodeMatcher(
         String.raw`(?:花云|滕王阁).*日本`
       );
+      EXPERIMENTAL_NODE_MATCHER = createCaseInsensitiveNodeMatcher(String.raw`实验性`);
       PROXY_GROUPS = {
         SELECT: "选择代理",
         MANUAL: "手动选择",
@@ -124,7 +125,7 @@ https://github.com/powerfullz/override-rules
         SSH: "SSH",
         SOGOU_INPUT: "搜狗输入法",
         AD_BLOCK: "广告拦截",
-        FLOWER_PREMIUM_ASIA: "花云高级-亚洲",
+        FLOWER_PREMIUM_ASIA: "花云高级-亚洲AI",
         SINGAPORE_FLOWER_TWG: "新加坡-花云+滕王阁",
         JAPAN_FLOWER_TWG: "日本-花云+滕王阁",
         GLOBAL: "GLOBAL",
@@ -326,22 +327,30 @@ https://github.com/powerfullz/override-rules
       {
         name: PROXY_GROUPS.SINGAPORE_FLOWER_TWG,
         icon: countriesMeta.新加坡.icon,
-        matcher: SINGAPORE_FLOWER_TWG_NODE_MATCHER
+        matcher: SINGAPORE_FLOWER_TWG_NODE_MATCHER,
+        excludeMatcher: EXPERIMENTAL_NODE_MATCHER
       },
       {
         name: PROXY_GROUPS.JAPAN_FLOWER_TWG,
         icon: countriesMeta.日本.icon,
-        matcher: JAPAN_FLOWER_TWG_NODE_MATCHER
+        matcher: JAPAN_FLOWER_TWG_NODE_MATCHER,
+        excludeMatcher: EXPERIMENTAL_NODE_MATCHER
       }
     ];
-    const matchNodes = (matcher) => nodes.filter((node) => matcher.regex.test(node.name || ""));
-    const activeCustomGroups = customGroups.map((group) => ({ ...group, nodes: matchNodes(group.matcher) })).filter((group) => regexFilter || group.nodes.length > 0);
+    const matchNodes = (matcher, excludeMatcher) => nodes.filter(
+      (node) => matcher.regex.test(node.name || "") && !excludeMatcher?.regex.test(node.name || "")
+    );
+    const activeCustomGroups = customGroups.map((group) => ({
+      ...group,
+      nodes: matchNodes(group.matcher, group.excludeMatcher)
+    })).filter((group) => regexFilter || group.nodes.length > 0);
+    const selectionProxies = [...activeCustomGroups.map((group) => group.name), ...defaultSelector];
     const groups = [
       {
         name: PROXY_GROUPS.SELECT,
         icon: `${CDN_URL}/gh/Koolson/Qure@master/IconSet/Color/Proxy.png`,
         type: "select",
-        proxies: [...activeCustomGroups.map((group) => group.name), ...defaultSelector]
+        proxies: selectionProxies
       },
       {
         name: PROXY_GROUPS.MANUAL,
@@ -349,14 +358,6 @@ https://github.com/powerfullz/override-rules
         "include-all": true,
         type: "select"
       },
-      ...activeCustomGroups.map(
-        (group) => buildGroupByType({
-          name: group.name,
-          icon: group.icon,
-          groupType,
-          nodeSource: regexFilter ? { "include-all": true, filter: group.matcher.pattern } : { proxies: group.nodes.map((node) => node.name).filter(isNotNull) }
-        })
-      ),
       landing ? {
         name: PROXY_GROUPS.FRONT_PROXY,
         icon: `${CDN_URL}/gh/Koolson/Qure@master/IconSet/Color/Area.png`,
@@ -518,7 +519,7 @@ https://github.com/powerfullz/override-rules
         name: PROXY_GROUPS.FINAL,
         icon: `${CDN_URL}/gh/Koolson/Qure@master/IconSet/Color/Final.png`,
         type: "select",
-        proxies: [PROXY_GROUPS.SELECT, "DIRECT"]
+        proxies: [PROXY_GROUPS.SELECT, ...selectionProxies]
       },
       {
         name: PROXY_GROUPS.AUTO,
@@ -544,6 +545,18 @@ https://github.com/powerfullz/override-rules
         groupType,
         nodeSource: !regexFilter ? { proxies: lowCostNodes.map((node) => node.name).filter(isNotNull) } : { "include-all": true, filter: LOW_COST_NODE_MATCHER.pattern }
       }) : null,
+      ...activeCustomGroups.map(
+        (group) => buildGroupByType({
+          name: group.name,
+          icon: group.icon,
+          groupType,
+          nodeSource: regexFilter ? {
+            "include-all": true,
+            filter: group.matcher.pattern,
+            ...group.excludeMatcher ? { "exclude-filter": group.excludeMatcher.pattern } : {}
+          } : { proxies: group.nodes.map((node) => node.name).filter(isNotNull) }
+        })
+      ),
       ...countryNames.map((country) => {
         const meta = countriesMeta[country];
         if (!meta) return null;
